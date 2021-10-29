a23ec0c0a66e5958bae6c0804f15607ca5658862
leon <leon.hammerla@gmx.de>
2021-10-29 15:56:53 +0200

    first try to parse whole corpus......some errors occur

--- src/convert_and_clean/uima_support.py
+++ src/convert_and_clean/uima_support.py
@@ -1,9 +1,15 @@
 import pathlib
 from datetime import datetime
-
+from multiprocessing import Pool
 import cassis
 import time
 import os
+from functools import partial
+from tqdm import tqdm
+
+
+
+
 # path of the corpus where all xmi files are stored
 XMI_CORPUS_PATH = "/vol/s5935481/parlamentary_xmi_corpus"
 # dictionary with identifiers for the different parliaments
@@ -99,7 +105,7 @@ def save_txt_as_xmi(txt_path:str, landtag:str, datum: str,
     return
 
 
-def save_directory_as_xmi(dir_path:str, mask_key:str, typesystem:cassis.TypeSystem):
+def save_directory_as_xmi(dir_path:str, mask_key:str, typesystem:str):
     """
     Function saves a whole directory of txt-files as xmi. it needs directory path, a typesystem for
     apache uima and a mask_key, which is ja identifier for the parliament of the given txt files.
@@ -108,37 +114,70 @@ def save_directory_as_xmi(dir_path:str, mask_key:str, typesystem:cassis.TypeSyst
     :param typesystem:
     :return:
     """
+    with open('/home/s5935481/work4/parliament_crawler/src/convert_and_clean/TypeSystem.xml', 'rb') as f:
+        typesystem = cassis.load_typesystem(f)
     files = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
     files = [file for file in files if os.path.isfile(file)]
+    fails = []
     for file in files:
         save_path = MASK[mask_key]["save_path"]
-        save_txt_as_xmi(
-                        txt_path=file,
-                        landtag=MASK[mask_key]["landtag"],
-                        datum=MASK[mask_key]["date_func"](file),
-                        typesystem=typesystem,
-                        user1=MASK[mask_key]["user1"],
-                        user2=MASK[mask_key]["user2"],
-                        origin_path=MASK[mask_key]["origin_path"],
-                        quelle=MASK[mask_key]["quelle"],
-                        subtilte_protocol=MASK[mask_key]["subtitle"](file),
-                        save_path=save_path
-                        )
+        try:
+            save_txt_as_xmi(
+                            txt_path=file,
+                            landtag=MASK[mask_key]["landtag"],
+                            datum=MASK[mask_key]["date_func"](file),
+                            typesystem=typesystem,
+                            user1=MASK[mask_key]["user1"],
+                            user2=MASK[mask_key]["user2"],
+                            origin_path=MASK[mask_key]["origin_path"],
+                            quelle=MASK[mask_key]["quelle"],
+                            subtilte_protocol=MASK[mask_key]["subtitle"](file),
+                            save_path=save_path
+                            )
+        except:
+            fails.append(file)
+    return fails
 
-
+def parse_and_save_whole_corpus(dir_path:str, mask_key:str, typesystem:str):
+    part_func = partial(save_directory_as_xmi, mask_key=mask_key, typesystem=typesystem)
+    dir_stack = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
+    dir_stack = [file_path for file_path in dir_stack if os.path.isdir(file_path)]
+    dir_with_txt = set()
+    while len(dir_stack) != 0:
+        dir_path = dir_stack[0]
+        sub_elems = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
+        sub_files, sub_dirs = [], []
+        for elem in sub_elems:
+            if os.path.isdir(elem):
+                sub_dirs.append(elem)
+            elif os.path.isfile(elem):
+                sub_files.append(elem)
+            else:
+                pass
+        dir_stack.extend(sub_dirs)
+        for sub_file in sub_files:
+            if ".txt" in sub_file:
+                dir_with_txt.add(dir_path)
+        dir_stack = dir_stack[1:]
+    dir_with_txt = list(dir_with_txt)
+    pool = Pool(28)
+    result = list(tqdm(pool.imap_unordered(part_func, dir_with_txt),
+                       desc="Parsing and Converting to XMI; Corpus: {}".format(mask_key)))
+    pool.close()
+    pool.join()
+    fails = []
+    for fail_list in result:
+        for fail in fail_list:
+            fails.append(fail)
+    for fail in fails:
+        print(fail + "\n")
+    return
 
 
 def main():
-    with open('/home/s5935481/work4/parliament_crawler/src/convert_and_clean/TypeSystem.xml', 'rb') as f:
-        typesystem = cassis.load_typesystem(f)
-
-    file = "/vol/s5935481/parlamentary_reichstag_text/1867 - 1895/1. Leg.-Periode/1871,1/22. Sitzung 25.04.1871.txt"
-    save_txt_as_xmi(
-        file,
-        MASK["Reichstag"]["landtag"], MASK["Reichstag"]["date_func"](file), typesystem,
-        MASK["Reichstag"]["user1"], MASK["Reichstag"]["user2"], MASK["Reichstag"]["origin_path"],
-        MASK["Reichstag"]["quelle"], MASK["Reichstag"]["subtitle"](file),
-        "/vol/s5935481/BIN")
+    typesystem = '/home/s5935481/work4/parliament_crawler/src/convert_and_clean/TypeSystem.xml'
+    dir_path = "/vol/s5935481/parlamentary_reichstag_text"
+    parse_and_save_whole_corpus(dir_path, "Reichstag", typesystem)
 
 if __name__ == "__main__":
     main()
