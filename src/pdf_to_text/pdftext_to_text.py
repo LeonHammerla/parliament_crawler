a640263f7619c346cecbf2820897b0c0523228fe
leon <leon.hammerla@gmx.de>
2021-11-04 18:36:09 +0100

    minor things?

--- src/pdf_to_text/pdftext_to_text.py
+++ src/pdf_to_text/pdftext_to_text.py
@@ -11,21 +11,34 @@ from multiprocessing import Pool
 from functools import partial
 
 
-def pdf_to_text(pdf_path:str) -> bool:
+def pdf_to_text(pdf_path:str, use_external_source:bool=True) -> bool:
     """
     Converts pdf to txt file
     :param pdf_path:
     :return:
     """
     success = False
-    try:
-        text = textract.process(pdf_path)
-        text = text.decode("utf-8")
-        with open(pdf_path.replace("pdf", "txt"), "w") as f:
-            f.write(text)
-        success = True
-    except:
-        success = False
+    if use_external_source:
+        try:
+            new_path = os.path.join(PATH, "/".join(pdf_path.split("/")[4:-1])).replace("pdf", "txt")
+            pathlib.Path(new_path).mkdir(parents=True, exist_ok=True)
+            text = textract.process(pdf_path)
+            text = text.decode("utf-8")
+            with open(os.path.join(new_path, pdf_path.split("/")[-1].replace("pdf", "txt")), "w") as f:
+                f.write(text)
+            success = True
+        except:
+            success = False
+    else:
+        try:
+            pathlib.Path("/".join(pdf_path.split("/")[:-1])).mkdir(parents=True, exist_ok=True)
+            text = textract.process(pdf_path)
+            text = text.decode("utf-8")
+            with open(pdf_path.replace("pdf", "txt"), "w") as f:
+                f.write(text)
+            success = True
+        except:
+            success = False
     return success
 
 
@@ -38,7 +51,10 @@ def dir_to_txt(dir_path:str) -> [bool]:
     files = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
     successes = []
     for file in tqdm(files, desc=f"Converting {dir_path.split('/')[-1]}"):
-        successes.append(pdf_to_text(file))
+        if ".pdf" in file:
+            successes.append(pdf_to_text(file))
+        else:
+            pass
     return successes
 
 
@@ -68,13 +84,13 @@ def dir_of_subdirs_to_txt(dir_path:str, forbidden_dirs:Union[list, None]) -> Non
             if ".pdf" in sub_file:
                 dir_with_pdf.add(dir_path)
         dir_stack = dir_stack[1:]
-    dir_with_txt = list(dir_with_pdf)
+    dir_with_pdf = list(dir_with_pdf)
     #print(dir_with_txt)
     if forbidden_dirs != None:
         for forbidden_dir in forbidden_dirs:
-            dir_with_txt.remove(forbidden_dir)
+            dir_with_pdf.remove(forbidden_dir)
     pool = Pool(14)
-    result = pool.map(dir_to_txt, dir_with_txt)
+    result = pool.map(dir_to_txt, dir_with_pdf)
     pool.close()
     pool.join()
     successes = []
@@ -92,4 +108,6 @@ def dir_of_subdirs_to_txt(dir_path:str, forbidden_dirs:Union[list, None]) -> Non
 
 
 if __name__ == "__main__":
-    dir_of_subdirs_to_txt("/vol/s5935481/parlamentary/sachsen_anhalt/pdf", None)
+    global PATH
+    PATH = "/resources/corpora/parlamentary_germany"
+    dir_of_subdirs_to_txt("/resources/corpora/parlamentary_germany/Berlin/pdf", None)
